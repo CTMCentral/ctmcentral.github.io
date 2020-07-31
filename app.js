5f57a4d5503be34c932df899c5a0fcec3cb3596c
jasonwynn10 <jasonwynn10@me.com>
2020-07-30 20:12:46 -0400

    Added Firebase login system

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// firebase init
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
 // DOM Elements
 
 const darkButton = document.getElementById('dark');
@@ -5,6 +10,29 @@ const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
 const body = document.body;
 
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+    }
+});
 
 // Apply the cached theme on reload
 
@@ -54,3 +82,59 @@ solarButton.onclick = () => {
         localStorage.setItem('isSolar', true);
     }
 };
+
+///// Firestore /////
+
+const db = firebase.firestore();
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+
+    if (user) {
+
+        // Database Reference
+        thingsRef = db.collection('things')
+
+        createThing.onclick = () => {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp()
+            });
+        }
+
+
+        // Query
+        unsubscribe = thingsRef
+            .where('uid', '==', user.uid)
+            .orderBy('createdAt') // Requires a query
+            .onSnapshot(querySnapshot => {
+
+                // Map results to an array of li elements
+
+                const items = querySnapshot.docs.map(doc => {
+
+                    return `<li>${doc.data().name}</li>`
+
+                });
+
+                thingsList.innerHTML = items.join('');
+
+            });
+
+
+
+    } else {
+        // Unsubscribe when the user signs out
+        unsubscribe && unsubscribe();
+    }
+});
